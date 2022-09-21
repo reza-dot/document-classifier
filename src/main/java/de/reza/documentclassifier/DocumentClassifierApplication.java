dcc77f3a92e0fdd692a9ff452d903417ff544b00
Reza <mo.reza97@gmail.com>
2022-09-21 13:30:11 +0200

    Initial commit

---
+++ src/main/java/de/reza/documentclassifier/DocumentClassifierApplication.java
@@ -0,0 +1,13 @@
+package de.reza.documentclassifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentClassifierApplication.class, args);
+	}
+
+}
