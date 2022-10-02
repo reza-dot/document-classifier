0eca578b15be354936c7348f984fb35719fc7446
Reza <mo.reza97@gmail.com>
2022-10-02 20:04:26 +0200

    Adding EuclideanDistance class. Adding units tests.

---
+++ src/main/java/de/reza/documentclassifier/classification/EuclideanDistance.java
@@ -0,0 +1,27 @@
+package de.reza.documentclassifier.classification;
+
+import de.reza.documentclassifier.pojo.Token;
+import de.reza.documentclassifier.utils.TextPositionSequence;
+
+public class EuclideanDistance {
+
+    /**
+     * Calculates the distance between two points
+     * @param tokenClass    Token of a class
+     * @param token         Token of given document
+     * @return              distance between two points
+     */
+    public static double calculateDistanceBetweenPoints(Token tokenClass, Token token) {
+        return Math.sqrt((tokenClass.getYAxis() - token.getYAxis()) * (tokenClass.getYAxis()- token.getYAxis()) + (tokenClass.getXAxis() - token.getXAxis()) * (tokenClass.getXAxis()  - token.getXAxis()));
+    }
+
+    /**
+     * Calculates the distance between two points
+     * @param hit    Token of a class
+     * @param token         Token of given document
+     * @return              distance between two points
+     */
+    public static double calculateDistanceBetweenPoints(TextPositionSequence hit, Token token) {
+        return Math.sqrt((hit.getY() - token.getYAxis()) * (hit.getY()- token.getYAxis()) + (hit.getX() - token.getXAxis()) * (hit.getX() - token.getXAxis()));
+    }
+}
---
+++ src/test/java/de/reza/documentclassifier/EuclideanDistanceTest.java
@@ -0,0 +1,25 @@
+package de.reza.documentclassifier;
+
+import de.reza.documentclassifier.classification.EuclideanDistance;
+import de.reza.documentclassifier.pojo.Token;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class EuclideanDistanceTest {
+
+    @Test
+    public void calculateDistanceBetweenTwoPoints(){
+
+        Token token1 = new Token("Apfelkuchen", 40.2f, 10.7f, 5f);
+        Token token2 = new Token("Butterkuchen", 70.9f, 1.7f, 19f);
+
+        double distance = EuclideanDistance.calculateDistanceBetweenPoints(token1, token2);
+        // rounded up to 2 decimal places
+        BigDecimal roundedDistance = new BigDecimal(distance).setScale(2, RoundingMode.HALF_UP);
+        Assertions.assertEquals(31.99,roundedDistance.doubleValue());
+    }
+
+}
