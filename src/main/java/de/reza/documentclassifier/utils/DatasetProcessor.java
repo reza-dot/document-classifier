9273163385a973d71e1c1d360b056158c651575d
Reza <mo.reza97@gmail.com>
2022-10-05 10:34:49 +0200

    Optimize code. Adding JavaDoc

--- src/main/java/de/reza/documentclassifier/utils/DatasetProcessor.java
+++ src/main/java/de/reza/documentclassifier/utils/DatasetProcessor.java
@@ -12,6 +12,12 @@ import java.nio.file.Paths;
 @Service
 public class DatasetProcessor {
 
+    /**
+     * Extract received training files (pdf files) in models/uuid/dataset. After extracting the zip file will be deleted
+     * @param dataset   Provided dataset as zip file
+     * @param uuid      uuid of the model
+     * @return          Path to the pdf files.
+     */
     public String unzip(MultipartFile dataset, String uuid){
 
         File dic = new File("models/" + uuid);
